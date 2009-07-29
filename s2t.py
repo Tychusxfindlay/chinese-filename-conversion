1ac4fbe6822d0651b9c83cf07440afbc36ef5cf4
David3.14159 <David3.14159@d9e5352a-6e51-11de-a63c-3560355a47d3>
2009-07-29 17:58:28 +0000

    solved chinese filename alignment problem

--- s2t.py
+++ s2t.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 from langconv import *
+import unicodedata
 import os,sys,glob
 
 def convertion(input):
@@ -12,9 +13,32 @@ if (len(sys.argv)==1):
 else:
   dir = sys.argv[1]
 
+def chinesewidth(str):
+  length = 0
+  str = str.decode('utf8')
+  for s in str:
+    length += 1
+    if unicodedata.east_asian_width(s) == 'W':
+      length += 1
+  return length
+
+def whitespace(width):
+  a=""
+  for i in xrange(width):
+    a=a+" "
+  return a
+
+def appendspace(str, width):
+  initialwidth = chinesewidth(str)
+  difference = width - initialwidth
+  if (difference < 0):
+    difference = 0
+  return str + whitespace(difference)
+
 filenames = glob.glob(dir)
 for filename in filenames:
-  print filename.ljust(50),  convertion(filename)
+  filename = os.path.basename(filename)
+  print appendspace(filename,40),  convertion(filename)
 
 input = raw_input("Confirm changes? y/n [y]:")
 if (input==''):
